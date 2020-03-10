8ee097f19cac69719ee5ff5d8742f12703508b73
Vadiraj S <vadiraj.s@slksoft.com>
2020-03-10 11:46:57 +0530

    changes

--- src/app/eve2020/eve2020.component.ts
+++ src/app/eve2020/eve2020.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {$} from 'jquery';
 @Component({
   selector: 'app-eve2020',
   templateUrl: './eve2020.component.html',
@@ -10,6 +10,15 @@ export class Eve2020Component implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+// $(document).ready(function() {
+//   $('.template-article img').each(function() {
+//       var currentImage = $(this);
+//       currentImage.wrap("<a class='image-link' href='" + currentImage.attr("src") + "'</a>");
+//   });
+//   $('.image-link').magnificPopup({type:'image'});  
+// });
+
   }
 
 }
